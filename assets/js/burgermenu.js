d9760b6c8177cb6cf16cb56c78e84d0e2cc7b2f0
mijam31 <156896989+mijam31@users.noreply.github.com>
2024-06-13 08:59:39 +0200

    konami code

--- assets/js/burgermenu.js
+++ assets/js/burgermenu.js
@@ -9,3 +9,37 @@ function closeul() {
   document.getElementById("burger").style.display = "none";
   document.querySelector(".navBurgermenu ul").style.display = "none";
 }
+
+const konamiCode = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a",
+];
+
+let konamiCodePosition = 0;
+
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+
+  if (key === konamiCode[konamiCodePosition]) {
+    konamiCodePosition++;
+
+    if (konamiCodePosition === konamiCode.length) {
+      activateSecret();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+});
+
+function activateSecret() {
+  const secretElement = alert("Srdjan, Fuck Dig :)");
+}
